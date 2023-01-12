7241c70dae5895fe7919ee11ad9b713a5b5cb04e
William Foucher <76762472+willfou193@users.noreply.github.com>
2023-01-12 14:38:00 -0500

    correction au last push

--- Assets/Plantes/cactus/cactus_will.cs
+++ Assets/Plantes/cactus/cactus_will.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class cactus_will : MonoBehaviour
 {
-    public GameObject cube;
     public GameObject parentBalle;
     public GameObject zoneInstanciable;
-    // Start is called before the first frame update
-    void Start()
-    {
-        gameObject.transform.LookAt(cube.transform);
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
