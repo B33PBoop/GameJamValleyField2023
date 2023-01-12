6e96eeaf20a3ef183d1368f021f3575545c5e7e8
Jean-Romain <77986513+B33PBoop@users.noreply.github.com>
2023-01-11 22:29:00 -0500

    Combinaison des scripts d'évolution et de tir

--- Assets/PlantsAI.cs
+++ Assets/PlantsAI.cs
@@ -3,31 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEditor.Rendering.FilterWindow;
 using static UnityEngine.GraphicsBuffer;
+using UnityEngine.UI;
 
 public class PlantsAI : MonoBehaviour
 {
     //le hero
     private GameObject player;
 
-    //element pour que l'enemie envoi des projectiles
+    //element pour que l'enemi envoie des projectiles
     public GameObject projectile;
     public GameObject projectileSpawn;
+    private float shootCD = 0;
+
+    //elements utilisés pour l'évolution de la plante
+    public float progress;
+    public GameObject plant;
+    public GameObject plantUI;
+    public GameObject plantProgressBar;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        progress = 0.05f;
         player = GameObject.FindGameObjectWithTag("Player");
-        InvokeRepeating("launchProjectile", 2f, 2f);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
         Vector3 targetPostition = new Vector3(player.transform.position.x,
         this.transform.position.y,
         player.transform.position.z);
         this.transform.LookAt(targetPostition);
+
+        if (progress < 1)
+        {
+            progress += 0.001f;
+            plant.transform.localScale = new Vector3(progress, progress, progress);
+            plantProgressBar.GetComponent<Slider>().value = progress;
+        }
+
+        if (progress >= 1 && shootCD <= 0)
+        {
+            Invoke("launchProjectile", 0f);
+            shootCD = 1f;
+        }
+
+        shootCD -= 0.01f;
     }
 
     public void launchProjectile()
